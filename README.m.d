99adcaad0bce6b796372062f2783cca316674352
yajairaalonso-stack <yajaira.alonso@catolica.edu.sv>
2026-06-25 21:13:52 -0600

    Add files via upload

---
+++ README.m.d
@@ -0,0 +1,31 @@
+# Snoopy Coffe
+
+# Snoopy Coffe  es uan acfeteria con tematica de Snoopy un personaje
+de un perrito blanco con manchas negra, que fue muy famoso en los anos
+ 80 gasta la alctualidad este personaje sigue siendo muy famoso en la ctualidad.
+
+ La pagina web fue creada con HTML5 y CSS con tematica de cafteria combiando con Snnopy
+ este proyecto fue creado por practicas de desarrollo web junior, lo cual buca combinar
+ un diseno con la experiencia del cliente.
+
+ Caracteristicas:
+ # Encabezado con logo de la cafteria y titulo 
+ # Tres secciones de cafes, postres, contacto 
+ que funciona como boton que redirecciona al cliente a esa seccion.
+ # Seccion de cafes con iamgenes y precios.
+ # seccion de postres con iamgenes y precios.
+ # Seccion de contactos con Gmail y telefono de la tienda.
+ # Footer esta cenyardo con un mesaje importante de derecho de autor 
+
+ ## UX/UI
+ #Jerarquia visual con precios y botones.
+ # Diseno con epacios en blanco y borde suaves.
+ # Sombra suave y zoom en productos.
+
+
+ ## Tecnologias:
+ # HTML5
+ # CSS
+
+
+
